89fcee801edd359eef276f49a7c1901b2e2916c7
Rodrigo Neves Trindade <rodrigonevest@gmail.com>
2025-12-22 21:46:57 -0300

    Ajustando debug

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-dev-key'
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
