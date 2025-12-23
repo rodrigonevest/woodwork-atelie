92e8dbb6c1ce0a77f02166447d37578be99c807c
Rodrigo Neves Trindade <rodrigonevest@gmail.com>
2025-12-22 21:58:00 -0300

    Ajustando settings2.1

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-dev-key')
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['woodwork-atelie.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1', 'localhost']
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
